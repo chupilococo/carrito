5553dd7c60edfae6111e3d4814751368a975780c
chupilococo <danilo.carena@davinci.edu.ar>
2019-04-23 18:40:46 -0300

    ident

--- secciones/productos.php
+++ secciones/productos.php
@@ -1,33 +1,25 @@
 <?php
-
 $productos = file_get_contents('assets/productos.json');
-
 $productos= json_decode($productos,true);
-
-//echo '<pre>';
-//var_dump($productos);
-
-
 $body='';
 
-echo '<pre>';
+//echo '<pre>';
 
 foreach ($productos as $id => $producto ){
-    $body .= "<div class='producto' ><h3>".$producto['producto']."</h3>
-                    <img src='https://unsplash.it/200/200' alt=''>
+    $body .= "<div class='producto' >
+                <h3>".$producto['producto']."</h3>
+                <img src='https://unsplash.it/200/200' alt=''>
                     <!--<p>".$producto['texto']."</p> -->   
               </div>";
 }
 
 //print_r($body);
 
-echo '</pre>';
+//echo '</pre>';
 
 $content=<<<CONTENT
-<section >
+<section class="productos" >
     <h2>Productos</h2>
-    <div class="productos">
         $body
-    </div>
 </section>
 CONTENT;
